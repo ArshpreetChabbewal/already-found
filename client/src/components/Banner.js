5f98d5dfdf37ca51cd6a6e944b80feb327e9a899
ArshpreetChabbewal <68906963+ArshpreetChabbewal@users.noreply.github.com>
2021-05-20 22:44:16 -0400

    Slight textual changes

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -6,11 +6,10 @@ const Banner = ({show, toggleForm}) => {
         <div className="banner">
             <h1 className="banner__header">Search with ease!</h1>
             <p className="banner__content">
-                Already found! is an easy and fast way to connect aspiring volunteers to their desired positions.
+                Already found is a fast and easy way to connect aspiring volunteers to their desired positions.
                 There are so many ways YOU can give back to the community, and help those in need.
                 Search through dozens of postings in only under a few minutes with our compact cards that flip as you 
                 hover over them, instantly viewing concise information about each position. 
-                So what are you waiting around here for?
             </p>
             <button className="banner__createPostButton" onClick={toggleForm}>
                 + Create Post
